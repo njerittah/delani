d49c31217829094393875c8e4c11f2419fd092d0
njerittah <vivianbianca92@gmail.com>
2020-02-21 21:05:25 +0300

    add array for days of week

---
+++ akhan/js/scripts.js
@@ -0,0 +1,5 @@
+
+
+  var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
+  var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
