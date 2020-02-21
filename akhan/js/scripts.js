faa46d5992628514f431f03a540757cdd196262e
njerittah <vivianbianca92@gmail.com>
2020-02-21 21:09:27 +0300

    add formulae into the js file

--- akhan/js/scripts.js
+++ akhan/js/scripts.js
@@ -3,3 +3,5 @@
   var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleName = ["Akosu", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Amna"];
   var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
+  var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
+  var dayOfBirth = Math.floor(dayOfBirth);
