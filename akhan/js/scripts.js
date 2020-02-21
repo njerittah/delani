a3bb0e9f323434c2ec457679e88417a848f9c767
njerittah <vivianbianca92@gmail.com>
2020-02-21 21:10:41 +0300

    add if function to the formulae

--- akhan/js/scripts.js
+++ akhan/js/scripts.js
@@ -5,3 +5,11 @@
   var dayOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sunday"];
   var dayOfBirth = ( ( (century/4) -2*year-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date ) % 7;
   var dayOfBirth = Math.floor(dayOfBirth);
+  if (gender === "Male") {
+       document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " and your name is " +  maleName[dayOfBirth -1];
+       }
+       else if (gender === "Female") {
+           document.getElementById("result").innerHTML = "You were born on " + dayOfWeek[dayOfBirth - 1]+ " and your name is " +  femaleName[dayOfBirth -1];
+       }
+
+}
